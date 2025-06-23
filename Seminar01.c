386a6cc9176291d8d9c60b521e9fd9214977b918
adrian.bogdan.tenea <adrian.bogdan.tenea@outlook.com>
2025-06-23 18:19:47 +0300

    Adaugarea citirii unei variabile reale.

--- Seminar01.c
+++ Seminar01.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 
 int main() {
+	//citirea unei variabile intregi
 	printf("Salutare!\nIntroduceti un numar intreg: ");
-	int variabila = 0;
-	scanf_s("%d", &variabila);
-	printf("Ai introdus: %d", variabila);
-	//citirea unei variabile reale.
+	int variabilaIntreaga = 0;
+	scanf_s("%d", &variabilaIntreaga);
+	printf("Ai introdus: %d", variabilaIntreaga);
+
+	//citirea unei variabile reale
+	printf("\nIntroduceti un numar real: ");
+	float variabilaReala = 0.0;
+	scanf_s("%f", &variabilaReala);
+	printf("Ati introdus: %5.2f", variabilaReala);
+
 	return 0;
 }
