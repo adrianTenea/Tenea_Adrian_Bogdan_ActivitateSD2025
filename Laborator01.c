6284ccc8c7f45a38a52ab190d4c0521a6562c504
adrian.bogdan.tenea <adrian.bogdan.tenea@outlook.com>
2025-07-01 11:42:14 +0300

    S-a adaugat functia de initializare pentru structura Telefon.

--- Laborator01.c
+++ Laborator01.c
@@ -9,3 +9,15 @@ struct Telefon {
 	float pret;
 	char serie;
 };
+
+//functia de initializare
+struct Telefon initializare(int id, int ram, const char* producator, float pret, char serie) {
+	struct Telefon t;
+	t.id = id;
+	t.RAM = ram;
+	t.producator = (char*)malloc(sizeof(char) * (strlen(producator) + 1));
+	strcpy_s(t.producator, strlen(producator) + 1, producator);
+	t.pret = pret;
+	t.serie = serie;
+	return t;
+}
